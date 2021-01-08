478da1e0b29904cd665a653b8ced69ce650e9c6f
KST <kst@gmail.com>
2021-01-08 19:05:29 +0530

    project code modified

---
+++ src/test/java/tomc/AppTest.java
@@ -0,0 +1,20 @@
+package tomc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
